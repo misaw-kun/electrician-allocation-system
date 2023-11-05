e9dfa48c35de3ac7881cd105f5a7039be0c68889
wasim <w.akram.1912@proton.me>
2023-11-05 21:06:40 +0530

    test: logging env vars on vercel

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -3,6 +3,16 @@ export const formatDate = (dateString: string): string => {
 };
 
 export async function getData(url: string) {
+  // for testing on vercel
+  console.log({
+    test: {
+      "X-Master-Key": `${process.env.API_MASTER_KEY}`,
+      "X-Access-Key": `${process.env.API_ACCESS_KEY}`,
+      site_bin: `${process.env.SITE_DATA_BIN}`,
+      elec_bin: `${process.env.ELECTRICIAN_DATA_BIN}`,
+    },
+  });
+
   const res = await fetch(url, {
     headers: {
       "X-Master-Key": `${process.env.API_MASTER_KEY}`,
