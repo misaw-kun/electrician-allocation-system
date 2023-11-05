a483df561ff413397128b0c25ee5c6d3003d1ba6
wasim <w.akram.1912@proton.me>
2023-11-05 19:54:21 +0530

    fix: turn off pre rendering

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { getData } from "./utils";
 import SiteDataTable from "./components/SiteDataTable";
 import AutoAssignButton from "./components/AutoAssign";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   //*/ FETCH: site and electrician data from server
   const electricianData: ElectricianData[] = await getData(
